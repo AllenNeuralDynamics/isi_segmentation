3d760eaca6c922564bad0259bca99b34aa412b21
dyf <david.feng@alleninstitute.org>
2025-06-26 15:52:15 +0000

    Changes in 14 files

--- code/run_predict.py
+++ code/run_predict.py
@@ -7,11 +7,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--hdf5_path', type=str, default=None, required=True, 
                         help='path to the hdf5 file which contains the testing sign map')
-    parser.add_argument('--sign_map_path', type=str, default=None, required=True, 
+    parser.add_argument('--sign_map_path', type=str, default='../results/sign_map.jpg', required=True, 
                         help='path to the sign map')
-    parser.add_argument('--label_map_path', type=str, default=None, required=True, 
+    parser.add_argument('--label_map_path', type=str, default='../results/label_map.png', required=True, 
                         help='path to save the label map')
-    parser.add_argument('--model_path', type=str, default=None, required=True, 
+    parser.add_argument('--model_path', type=str, default='../data/isi_segmentation_model/isi_segmentation_model.h5', required=True, 
                         help='path to the trained isi-segmentation model')
     
     args = parser.parse_args()
