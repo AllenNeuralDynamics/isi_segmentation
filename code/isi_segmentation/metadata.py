dbdec858606c7cd433674b7bdfc3057c9a3dea0b
arielleleon <ariellel@alleninstitute.org>
2026-01-08 00:08:36 +0000

    fix: save for a minute

--- code/isi_segmentation/metadata.py
+++ code/isi_segmentation/metadata.py
@@ -9,11 +9,10 @@ from aind_data_schema.core.quality_control import (QCEvaluation, QCMetric,
                                                    QCStatus, QualityControl,
                                                    Stage, Status)
 from aind_data_schema_models.modalities import Modality
-from aind_metadata_upgrader.data_description_upgrade import \
     
 
 
-def make_data_description(input_dir="../data") -> DataDescriptionUpgrade:
+def make_data_description(input_dir="../data") -> DerivedDataDescription:
     """Read data_description.json from input directory.
 
     Parameters
@@ -23,8 +22,8 @@ def make_data_description(input_dir="../data") -> DataDescriptionUpgrade:
 
     Returns
     -------
-    DataDescriptionUpgrade
-        An upgraded DataDescriptionUpgrade object based on the existing data description.
+    DerivedDataDescription
+        An upgraded DerivedDataDescription object based on the existing data description.
     """
     data_description = None
 
@@ -33,9 +32,7 @@ def make_data_description(input_dir="../data") -> DataDescriptionUpgrade:
         if data_desc_path.exists():
             with open(data_desc_path, "r") as f:
                 data_description = json.load(f)
-    derived_data_description = DataDescription.from_raw(
-            DataDescription(data_description), process_name="processed"
-        )
+    derived_data_description = DerivedData
     data_upgrader = data_description_upgrader.upgrade()
     data_upgrader.modality = [Modality.ISI]
 
